8e41febfc0d51394698ecfaa4ed8d5301131e58e
noe132 <nnnnoe132@gmail.com>
2017-05-08 13:24:55 +0800

    chat

--- src/vue/chat.vue
+++ src/vue/chat.vue
@@ -2,19 +2,38 @@
 <div>
     <div class="chat-background chat-background-color"></div>
     <div class="chat-background chat-background-image"></div>
-    <div class="main">
-        <div class="channel-box">
+    <div class="chat-container flex">
+        <div class="channel-box flex">
+            <div class="channel-header flex">
+                <div class="title">CHANNEL LIST</div>
+                <i class="add-channel">
 
+                </i>
+            </div>
+            <div class="channel-body flex flexcolumn">
+            </div>
         </div>
-        <div class="chat-box">
+        <div class="chat-box flex flexcolumn">
+            <div class="chat-header flex">
+                <div class="title">
+                    noe132
+                </div>
+                <div class="button-box flex">
+                    <div class="button-item logout"></div>
+                </div>
+            </div>
+            <div class="chat-main flex">
+                <div class="chat-content flex">
+                    <div class="chat-area"></div>
+                    <div class="chat-input-box">
 
-        </div>
-        <div class="friend-box">
-
+                    </div>
+                </div>
+                <div class="friend-box flex">
+                </div>
+            </div>
         </div>
     </div>
-
-    <router-view></router-view>
 </div>
 </template>
 
@@ -28,3 +47,8 @@ module.exports = {
     }
 };
 </script>
+
+
+<style scoped lang="less">
+@import "/src/less/chat.less";
+</style>
